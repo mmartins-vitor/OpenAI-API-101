4629521cbd0e5e11b9ccca849dbbe8aa25e2ba30
vitor-inovacao <vitor.inovacao@emap.ma.gov.br>
2025-02-26 16:10:21 -0300

    replace words

--- openAI-101.py
+++ openAI-101.py
@@ -1,5 +1,4 @@
 # Importações
-
 from dotenv import load_dotenv
 from openai import OpenAI
 import os
@@ -8,15 +7,23 @@ import os
 load_dotenv()
 api_key = os.getenv("API_KEY")
 
+prompt="""Replace car with plane and adjust phrase: A car is a vehicle that is typically 
+powered by an internal combustion engine or an electric motor. It has four wheels, and is 
+designed to carry passengers and/or cargo on roads or highways. Cars have become a ubiquitous 
+part of modern society, and are used for a wide variety of purposes, such as commuting, travel, 
+and transportation of goods. Cars are often associated with freedom, independence, and mobility."""
+
 # Create OpenAI Client
 client = OpenAI(api_key=api_key)
 response = client.chat.completions.create(
     model="gpt-4o-mini",
-    store=True,
+    #store=True,
     messages=[
-        {"role": "user", "content": "Meu marido, Vitor, esta me traindo?"}
-    ]
+        {"role": "user", "content": prompt}
+    ],
+    max_tokens=100
 )
 
+# 
 print(response.choices[0].message.content)
 
