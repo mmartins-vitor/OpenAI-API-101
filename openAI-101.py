ca1b4b8aa5e51a0f6a0ad0956e4012a0dff2f152
vitor-inovacao <vitor.inovacao@emap.ma.gov.br>
2025-02-27 22:22:46 -0300

    how to stores responses to context a chat

--- openAI-101.py
+++ openAI-101.py
@@ -7,29 +7,33 @@ import os
 load_dotenv()
 api_key = os.getenv("API_KEY")
 
-prompt="""Replace car with plane and adjust phrase: A car is a vehicle that is typically 
-powered by an internal combustion engine or an electric motor. It has four wheels, and is 
-designed to carry passengers and/or cargo on roads or highways. Cars have become a ubiquitous 
-part of modern society, and are used for a wide variety of purposes, such as commuting, travel, 
-and transportation of goods. Cars are often associated with freedom, independence, and mobility."""
-
 # Create OpenAI Client
 client = OpenAI(api_key=api_key)
-response = client.chat.completions.create(
-    model="gpt-4o-mini",
-    # Add a user and assistant message for in-context learning
-    messages=[
-        {"role": "system",
-        "content": "You are a helpful Python programming tutor."},
-        {"role": "user",
-        "content": "Explain what the min() function does."},
-        {"role": "assistant",
-        "content": "A função min() retorna o menor item de um iterável."},
-        {"role": "user",
-        "content": "Explain what the type() function does."}
-        ],
-)
 
-# 
-print(response.choices[0].message.content)
+messages = [{"role": "system", "content": "You are a helpful math tutor."}]
+user_msgs = ["Explain what pi is.", "Summarize this in two bullet points."]
 
+for q in user_msgs:
+    # print("User: ", q)
+
+    # Create a dictionary for the user message from q and append to messages
+    user_dict = {"role":"user", "content": q}
+    messages.append(user_dict)
+
+    # Create de API request
+    response = client.chat.completions.create(
+        model = "gpt-4o-mini",
+        messages=messages,
+        max_tokens=100
+    )
+
+    # Convert the assistant's message to a dict and append to message
+    assistant_dict = {
+        "role": "assistant",
+        "content": response.choices[0].message.content
+    }
+
+    messages.append(assistant_dict)
+    print("Assistant: ", response.choices[0].message.content, "\n")
+
+print(messages)
